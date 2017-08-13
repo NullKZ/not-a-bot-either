2da1a910caa7976af9fcc5dc5471ff80240e4be4
Kevin Zou <kevinzou2150@gmail.com>
2017-08-12 23:30:07 -0400

    added queue, doesnt work

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ tokens = tokenReader['DEFAULT']
 class vc:
     def __init__(self):
         self.voiceCh = None
+        self.textCh = None
         self.player = None
         self.vol = 100
         self.active = False
+        self.queue = []
 
 servers = {}
 
@@ -43,30 +45,44 @@ async def on_message(message):
         await client.send_message(message.channel, 'pong!')
     if message.content.startswith('!kill'):
         await client.logout()
+    if message.content.startswith('!queue'):
+        msg = '```\nCurrent Queue:\n'
+        n = 1
+        for i in servers[message.server.id].queue:
+            msg+=(str(n)+'. '+i[1]+'\n')
+        msg+='```'
+        await client.send_message(message.channel, string)
     if message.content.startswith('!play'):
         if not message.server.id in servers:
             servers[message.server.id] = vc()
+        servers[message.server.id].textCh = message.channel
         try:
             url = await ytsearch(message.content.split(' ', 1)[1])
             if servers[message.server.id].voiceCh == None:
                 servers[message.server.id].voiceCh = await client.join_voice_channel(message.author.voice.voice_channel)
             if servers[message.server.id].player:
                 if not servers[message.server.id].player.is_playing():
-                    servers[message.server.id].player = await voiceCh.create_ytdl_player(url[0])
+                    print('playing')
+                    servers[message.server.id].player = await servers[message.server.id].voiceCh.create_ytdl_player(url[0], after=lambda: endAudio(servers[message.server.id]))
                 else:
-                    pass #queue it up
+                    print('queue test')
+                    servers[message.server.id].queue.append(url)
+                    await client.send_message(message.channel, '`Queued: '+url[1]+'.`')
+                    return
             else:
-                servers[message.server.id].player = await voiceCh.create_ytdl_player(url[0], after=print('song end'))
+                servers[message.server.id].player = await servers[message.server.id].voiceCh.create_ytdl_player(url[0], after=lambda: endAudio(servers[message.server.id]))
             servers[message.server.id].player.start()
             if servers[message.server.id].vol:
-                servers[message.server.id].player.volume = vol/100
+                servers[message.server.id].player.volume = servers[message.server.id].vol/100
             await client.send_message(message.channel, '`Now Playing: '+url[1]+'.`')
         except Exception as e:
             print(e.__class__.__name__)
+            print(e)
     if message.content.startswith('!dc'):
         try:
             await servers[message.server.id].voiceCh.disconnect()
             servers[message.server.id].voichCh = None
+            servers[message.server.id].player.stop()
             servers[message.server.id].player = None
         except:
             await client.send_message(message.channel, '`Not Connected to a Voice Channel.`')
@@ -98,12 +114,20 @@ async def ytsearch(search):
     result = json.JSONDecoder().decode(requests.get('https://www.googleapis.com/youtube/v3/search', params = query).text)
     for i in result['items']:
         if i['id']['kind'] == 'youtube#video':
-            return 'https://www.youtube.com/watch?v='+i['id']['videoId'],i['snippet']['title']
+            return 'http://www.youtube.com/watch?v='+i['id']['videoId'],i['snippet']['title']
 async def updateStatus():
     status = ' on '+str(len(client.servers))+' server'
     if len(client.servers) > 1:
         status+='s'
     await client.change_presence(game=discord.Game(name=status))
 
-
+def endAudio(currentvc):
+    print(len(currentvc.queue))
+    if len(currentvc.queue) > 0:
+        url = currentvc.queue.shift()
+        currentvc.player = currentvc.voiceCh.create_ytdl_player(url[0], after=lambda: endAudio(currentvc))
+        currentvc.player.start()
+        if currentvc.vol:
+            currentvc.player.volume = servers[message.server.id].vol/100
+        client.send_message(currentvc.textCh, '`Now Playing Queued Song: '+url[1]+'.`')
 client.run(tokens['discord'])
