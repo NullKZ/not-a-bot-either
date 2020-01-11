4f374f685401f04bc78f0bc13f085c9854de552b
Kevin Zou <kevinzou2150@gmail.com>
2020-01-11 15:45:10 -0500

    include new counter

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from configparser import ConfigParser
 import discord
 import asyncio
-import requests
+#import requests
 import json
 import sys
+import random
+#FEATURES
+counters = True
 
+#CODE
 tokenReader = ConfigParser()
 tokenReader.optionxform = str
 tokenReader.read('tokens.ini')
 tokens = tokenReader['DEFAULT']
 active = False
 client = discord.Client()
+join = lambda x: sum(x,[])
+delims = 'd+-'
 
 @client.event
 async def on_ready():
@@ -22,47 +28,66 @@ async def on_message(message):
     global active
     if message.author == client.user:
         return
-    if message.content.lower().startswith("!charcount"):
+    if False: #message.content.lower().startswith("-roll"):
+        args = [message.content.lower()[5:].replace(" ","")]
+        for delim in delims:
+            args = join(f.split(delim) for f in args)
+        try:
+            numdice = int(args[0])
+            dicesize = int(args[1])
+            bonus = 0
+        except Exception:
+            await client.send_message(message.channel, "Invalid input.")
+        try:
+            bonus = int(args[2])
+        except Exception:
+            pass
+        sign = " +"
+        if "-" in message.content.lower()[5:].replace(" ",""):
+            sign = " -"
+        output = "Rolling "+str(args[0])+"d"+str(args[1])+sign+str(bonus)
+        output+=" | `"
+        outlist = []
+        for i in range(numdice):
+            outlist.append(random.randint(1,dicesize))
+        if "-" in message.content.lower()[5:].replace(" ",""):
+            output+=str(outlist)[1:-1].replace(",","")+" -"+str(bonus)+"`\n"
+            output+=str(sum(outlist)-bonus)
+        else:
+            output+=str(outlist)[1:-1].replace(",","")+" +"+str(bonus)+"`\n"
+            output+=str(sum(outlist)+bonus)
+        await client.send_message(message.channel, output)
+    if message.content.lower().startswith("!bads"):
+        if not counters:
+            await message.channel.send(content="no u. (Command Disabled.)")
+            return
         if active:
-            await client.send_message(message.channel, "Already counting. Command ignored.")
+            await message.channel.send(content="Already Counting.")
             return
         active=True
-        edit = await client.send_message(message.channel, "Counting Characters...")
-        tc={}
-        num=0
-        async for msg in client.logs_from(message.channel, limit=400000):
-            num+=1
-            if num%71==0:
-                print("\r"+str(num), end="")
-            if not str(msg.author) in tc:
-                tc[str(msg.author)] = 0
-            tc[str(msg.author)] += len(msg.content)
-        baseString = "Messages Sent in Total:\n```"
-        for key, value in tc.items():
-            baseString += (key+": "+str(value)+"\n")
-        baseString+="```"
-        await client.edit_message(edit, new_content=baseString)
-        active=False
-        print("")
-    if message.content.lower().startswith("!test"):
-        if active:
-            await client.send_message(message.channel, "Already counting. Command ignored.")
-            return
-        active=True
-        edit = await client.send_message(message.channel, "Counting Bads...")
+        await message.channel.send(content="Counting Bads...")
         fc={}
+        dc={}
         sc={}
         tc={}
         num=0
-        async for msg in client.logs_from(message.channel, limit=400000):
+        async for msg in message.channel.history(limit = None):
             num+=1
-            if num%71==0:
+            if num%100==0:
                 print("\r"+str(num), end="")
-            if "fuck" in msg.content.lower():
+            if not str(msg.author) in tc:
+                tc[str(msg.author)] = 0
+            tc[str(msg.author)] += 1
+            content = msg.content.lower()
+            if "fuck" in content or "fuk" in content or "fuq" in content or "fak" in content:
                 if not str(msg.author) in fc:
                     fc[str(msg.author)] = 0
                 fc[str(msg.author)] +=1
-            if "shit" in msg.content.lower():
+            if "dam" in content or "damn" in content:
+                if not str(msg.author) in dc:
+                    dc[str(msg.author)] = 0
+                dc[str(msg.author)] +=1
+            if "shit" in content:
                 if not str(msg.author) in sc:
                     sc[str(msg.author)] = 0
                 sc[str(msg.author)] +=1
@@ -72,8 +97,14 @@ async def on_message(message):
         baseString+= "```Shits Taken:\n```"
         for key, value in sc.items():
             baseString += (key+": "+str(value)+"\n")
+        baseString+= "```Gods Damned:\n```"
+        for key, value in dc.items():
+            baseString += (key+": "+str(value)+"\n")
+        baseString+= "```Messages Sent:\n```"
+        for key, value in tc.items():
+            baseString += (key+": "+str(value)+"\n")
         baseString+="```"
-        await client.edit_message(edit, new_content=baseString)
-        print("")
+        print(baseString)
+        print("Done!")
         active = False
 client.run(tokens['discord'])
